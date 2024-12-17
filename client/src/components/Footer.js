3e0921357776c2c033234f23e3d0e20e85d7eb5d
Enmanuel1700 <162062821+Enmanuel1700@users.noreply.github.com>
2024-12-16 19:31:40 -0500

    Update Footer.js
    Dynamically Update Footer Year

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,15 +1,11 @@
-const Footer = () => (
-    <footer
-        style={{
-            textAlign: 'center',
-            marginTop: '20px',
-            padding: '10px 0',
-            color: '#777',
-            fontSize: '0.9rem',
-        }}
-    >
-        © 2024 To-Do App. Built with ❤️ by Enmanuel Santana.
-    </footer>
-);
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer style={{ textAlign: 'center', marginTop: '20px', color: '#777' }}>
+            © {currentYear} To-Do App. Built with ❤️ by [Enmanuel Santana].
+        </footer>
+    );
+};
 
 export default Footer;
